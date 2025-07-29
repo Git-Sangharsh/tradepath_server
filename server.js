88770dfea705ed5533c02e35acc2d773b8c71f97
sangharsh <sangharshdev69@gmail.com>
2025-07-29 18:11:57 +0530

    redeploy cause render is case Sensitive

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import journalRoutes from "./routes/journal.js";
+import journalRoutes from "./routes/Journal.js";
 import authRoutes from "./routes/Auth.js";
 
 
